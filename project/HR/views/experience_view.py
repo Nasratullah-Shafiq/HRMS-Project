24e20764ef338c7f2e8f1e7cda7307e845e31886
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-09-14 18:58:22 +0430

    University, Degree, Faculty and Major views deleted and is integrated in Education_view

--- project/HR/views/experience_view.py
+++ project/HR/views/experience_view.py
@@ -147,66 +147,81 @@ def jobPositionApi(request, id=0):
 def gradeApi(request, id=0):
     if request.method == 'GET':
         # Get all Grades
-        job_position = JobPosition.objects.all()
-        job_position_serializer = JobPositionSerializer(job_position, many=True)
-        return JsonResponse(job_position_serializer.data, safe=False)
+        grade = Grade.objects.all()
+        grade_serializer = GradeSerializer(grade, many=True)
+        return JsonResponse(grade_serializer.data, safe=False)
     
     elif request.method == 'POST':
         # Add a new employee
-        job_position_data = JSONParser().parse(request)
-        job_position_serializer = JobPositionSerializer(data=job_position_data)
-        if job_position_serializer.is_valid():
-            job_position_serializer.save()
+        grade_data = JSONParser().parse(request)
+        grade_serializer = GradeSerializer(data=grade_data)
+        if grade_serializer.is_valid():
+            grade_serializer.save()
             return JsonResponse("Employee Added Successfully", safe=False)
         return JsonResponse("Failed to Add", safe=False)
     
     elif request.method == "PUT":
         # Update an existing employee
-        job_position_data = JSONParser().parse(request)
+        grade_data = JSONParser().parse(request)
         try:
-            job_position = JobPosition.objects.get(id=id)
-        except JobPosition.DoesNotExist:
+            grade = Grade.objects.get(id=id)
+        except Grade.DoesNotExist:
             return JsonResponse("Employee not found", safe=False, status=404)
 
-        job_position_serializer = JobPositionSerializer(job_position, data=job_position_data)
-        if job_position_serializer.is_valid():
-            job_position_serializer.save()
+        job_position_serializer = GradeSerializer(grade, data=grade_data)
+        if grade_serializer.is_valid():
+            grade_serializer.save()
             return JsonResponse("Updated Successfully", safe=False)
         return JsonResponse("Failed to Update", safe=False)
     
     elif request.method == "DELETE":
         # Delete an employee
         try:
-            job_position = JobPosition.objects.get(id=id)
-        except JobPosition.DoesNotExist:
+            grade = Grade.objects.get(id=id)
+        except Grade.DoesNotExist:
             return JsonResponse("Employee not found", safe=False, status=404)
 
-        job_position.delete()
+        grade.delete()
         return JsonResponse("Deleted Successfully", safe=False)
     
 
 @csrf_exempt
-def stepApi(request,id=0):
-    if request.method=='GET':
-        organization_type = OrganizationType.objects.all()
-        organization_type_serializer=OrganizationTypeSerializer(organization_type,many=True)
-        return JsonResponse(organization_type_serializer.data,safe=False)
-    elif request.method=='POST':
-        organization_type_data=JSONParser().parse(request)
-        organization_type_serializer=OrganizationTypeSerializer(data=organization_type_data)
-        if organization_type_serializer.is_valid():
-            organization_type_serializer.save()
-            return JsonResponse("Student Added Successfully",safe=False)
-        return JsonResponse("Failed to Add",safe=False)
-    elif request.method=="PUT":
-        organization_type_data=JSONParser().parse(request)
-        organization_type=OrganizationType.objects.get(id=id)
-        organization_type_serializer=OrganizationTypeSerializer(organization_type_data=organization_type_data)
-        if organization_type_serializer.is_valid():
-            organization_type_serializer.save()
-            return JsonResponse("Updated Successfully",safe=False)
-        return JsonResponse("Failed to Update")
-    elif request.method=="DELETE":
-        organization_type=organization_type.objects.get(id=id)
-        organization_type.delete()
-        return JsonResponse("Deleted Successfully",safe=False)
+def stepApi(request, id=0):
+    if request.method == 'GET':
+        # Get all Steps
+        step = Step.objects.all()
+        step_serializer = StepSerializer(step, many=True)
+        return JsonResponse(step_serializer.data, safe=False)
+    
+    elif request.method == 'POST':
+        # Add a new Steps
+        step_data = JSONParser().parse(request)
+        step_serializer = StepSerializer(data=step_data)
+        if step_serializer.is_valid():
+            step_serializer.save()
+            return JsonResponse("Employee Added Successfully", safe=False)
+        return JsonResponse("Failed to Add", safe=False)
+    
+    elif request.method == "PUT":
+        # Update an existing Steps
+        step_data = JSONParser().parse(request)
+        try:
+            grade = Step.objects.get(id=id)
+        except Step.DoesNotExist:
+            return JsonResponse("Employee not found", safe=False, status=404)
+
+        step_serializer = GradeSerializer(grade, data=step_data)
+        if step_serializer.is_valid():
+            step_serializer.save()
+            return JsonResponse("Updated Successfully", safe=False)
+        return JsonResponse("Failed to Update", safe=False)
+    
+    elif request.method == "DELETE":
+        # Delete an Steps
+        try:
+            step = Step.objects.get(id=id)
+        except Step.DoesNotExist:
+            return JsonResponse("Employee not found", safe=False, status=404)
+
+        step.delete()
+        return JsonResponse("Deleted Successfully", safe=False)
