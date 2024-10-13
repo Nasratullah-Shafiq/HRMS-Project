118d265d032058d4de6202243aa3b412d9292695
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-10-13 20:26:16 +0430

    Home modified

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 function Home(){
     return(
-        <div className = "container">
+        <div className = "container" style={{ background: 'red'}}>
             <h1> Home Page </h1>
         </div>
     )
