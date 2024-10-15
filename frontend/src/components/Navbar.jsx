e02dcf3fbaf457abba6fcd7cdd3f6e7ba21afa61
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-10-15 09:04:48 +0430

    navbar modified

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -207,7 +207,7 @@ function Navbar(){
   }
     return(
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container">
+        <div className="container-fluid">
           <Link className="navbar-brand" to="#">React</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
