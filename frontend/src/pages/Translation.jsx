0ecb1f1e5b41564221065dc1933fd35e582b23e9
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-12-11 20:21:26 +0430

    translation files added

--- frontend/src/pages/Translation.jsx
+++ frontend/src/pages/Translation.jsx
@@ -1,16 +1,11 @@
-// // src/App.js
-// import React from 'react';
-// import './i18n/i18n'; // Import the i18n configuration
+// src/App.js
+import React from 'react';
+import '../i18n/i18n'; // Import the i18n configuration
 
-// import { useTranslation } from 'react-i18next';
-
-// import './i18n/i18n'; // Import the i18n configuration
-import '../i18n/i18n';
 import { useTranslation } from 'react-i18next';
 
-
-function Translation() {
-  const { t, i18n } = useTranslation();
+export default function Translation() {
+  const { t, i18n } = useTranslation()
 
   // Function to handle language change
   const changeLanguage = (lng) => {
@@ -18,13 +13,44 @@ function Translation() {
   };
 
   return (
-    <div className="App">
-      <h1>{t('welcome')}</h1>
-      <p>{t('description')}</p>
-      <button onClick={() => changeLanguage('en')}>English</button>
-      <button onClick={() => changeLanguage('fr')}>French</button>
-    </div>
+    <div className="">
+      <select onClick ={(e)=>changeLanguage(e.target.value)}>
+        <option value="en"> English </option>
+        <option value="dr"> Dari </option>
+      </select>
+      <div>{t('welcome_message')}</div>
+   </div>
   );
 }
 
-export default Translation;
+
+
+
+
+// // src/App.jsx
+// import React, { useState, useEffect } from "react";
+// import { getTranslations } from '../i18n/i18n';
+
+// const App = () => {
+//   const [language, setLanguage] = useState("en"); // Default language
+//   const [translations, setTranslations] = useState(getTranslations(language));
+
+//   // Update translations when the language changes
+//   useEffect(() => {
+//     setTranslations(getTranslations(language));
+//   }, [language]);
+
+//   const handleLanguageChange = (event) => {
+//     setLanguage(event.target.value); // Update language state
+//   };
+
+  
+// };
+
+// export default App;
+
+
+
+
+
+// export default Translation;
