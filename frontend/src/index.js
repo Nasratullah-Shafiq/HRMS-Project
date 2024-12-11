2c08dbcb9e419be085b065a9de2c153f56f0499f
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2024-12-11 20:19:38 +0430

    index files updated

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -17,19 +17,42 @@
 // reportWebVitals();
 
 
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { BrowserRouter } from 'react-router-dom';
+// import './i18n/i18n'; // Ensure i18n is initialized here
+// import { I18nextProvider } from 'react-i18next';
+// import i18n from './i18n/i18n'; // Import initialized i18n instance
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   // <React.StrictMode>
+//     <BrowserRouter>
+//         <App />
+//     </BrowserRouter>
+    
+//   // </React.StrictMode>
+// );
+
+
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './i18n/i18n'; // Initialize i18n
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n/i18n'; // Import i18n instance
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-    
-  // </React.StrictMode>
+  <I18nextProvider i18n={i18n}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </I18nextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
