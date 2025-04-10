3c5772d16529b71c9d8331465bcec650a3ed2cc4
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-04-10 21:43:59 +0430

    new changes

---
+++ frontend/src/components/Employeeinfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function EmployeeInfo() {
+  return (
+    <div>
+      <h2>Employee Information</h2>
+      <p>This is the Employee Information page.</p>
+    </div>
+  );
+}
+
+export default EmployeeInfo;
