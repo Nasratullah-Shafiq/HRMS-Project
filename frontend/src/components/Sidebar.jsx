a8b0ae9ddebd0aae4592ffbad44a36567b9369a3
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-04-14 15:29:00 +0430

    new changes

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ function Sidebar(){
         <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
         <svg class="bi pe-none me-2" width="40" height="32"></svg>
         <span class="fs-4">Sidebar</span>
-        </Link>
+        </Link> 
         <hr/>
         <ul className="nav nav-pills flex-column mb-auto">
         <li className="nav-item">
