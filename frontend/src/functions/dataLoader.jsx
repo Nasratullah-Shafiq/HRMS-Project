d5e53d12c1b981e1bca496e2800c2f9f1152a475
Nasratullah-Shafiq <nasratullah.shafiq@gmail.com>
2025-08-03 20:18:44 +0430

    dataloader file updated

--- frontend/src/functions/dataLoader.jsx
+++ frontend/src/functions/dataLoader.jsx
@@ -2,31 +2,31 @@
 
 import axios from 'axios';
 
-export async function loadData(endpoint) {
+export async function loadData(endpoint, toast) {
   try {
     const result = await axios.get(`http://127.0.0.1:8000/${endpoint}/`);
     return result.data;
   } catch (err) {
-    console.error('Error fetching Data:', err);
+    toast.error('Error fetching Data:', err);
     throw err; // Rethrow to handle it in the component
   }
 }
 
 
 // Save all data via this function 
-async function saveData(event, endpoint) {
-    event.preventDefault();
-    try {
-      await axios.post(`http://127.0.0.1:8000/${endpoint}/`, {
-        id: id,
-        // name: name
-      });
-      toastr.success("Record Registered Successfully");
-      setId("");
-      setName("");
-      loadData();
-    } catch (err) {
-      toastr.error("Faculty Registration Failed");
-    }
-  }
+// async function saveData(event, endpoint) {
+//     event.preventDefault();
+//     try {
+//       await axios.post(`http://127.0.0.1:8000/${endpoint}/`, {
+//         id: id,
+//         // name: name
+//       });
+//       toastr.success("Record Registered Successfully");
+//       setId("");
+//       setName("");
+//       loadData();
+//     } catch (err) {
+//       toastr.error("Faculty Registration Failed");
+//     }
+//   }
   
